c3809db8b9bead174028bcd34ae00f941d829999
Odalys Paz Mendoza <odhalyxzpaz@gmail.com>
2020-06-28 19:59:52 -0500

    Creacion del formulacio para agregar kdramas

---
+++ src/app/models/Drama.model.ts
@@ -0,0 +1,11 @@
+export class DramaModel {
+    title: string;
+    episodes: string;
+    release: string;
+    gender: string;
+    network: string;
+    writer: string;
+    director: string;
+    constructor() {
+    }
+}
